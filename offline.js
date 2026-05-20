c70f88f78015c99487f7f629f2abfba05b8f6ebb
dev-lucido <pasindu@lucido.life>
2026-05-20 15:44:10 +0530

    ddd

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1779271485,
+	"version": 1779271931,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -31,6 +31,8 @@
 		"images/popupbuttonssprite-sheet0.png",
 		"images/popupbuttonssprite-sheet1.png",
 		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
